e0b43fcfad565bdffb354bc3ad4754797e1677ce
Igor Pestretsov <ipestretsov@ptsecurity.com>
2022-05-08 14:18:03 +0300

    bug fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -11,8 +11,8 @@ import (
 )
 
 type Config struct {
-	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"localhost:35217"`
-	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:35217"`
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
+	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 }
 
 func main() {
